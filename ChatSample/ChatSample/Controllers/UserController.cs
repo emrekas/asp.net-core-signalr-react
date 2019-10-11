7d89e01f8496cf52b4e902de185fa0b8f68d8834
Yunus Emre KAŞ <y.emrekas@outlook.com>
2019-10-11 10:12:00 +0300

    gitignore eklendi

---
+++ ChatSample/ChatSample/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSample.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+    }
+}
